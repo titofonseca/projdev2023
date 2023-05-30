2c0688031c775a4778f01f86f31df6e86cd06ea6
titofonseca <104699484+titofonseca@users.noreply.github.com>
2023-05-30 22:47:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+// Define UI elements
+const urlField = document.getElementById('apiUrl');
+const fetchDataBtn = document.getElementById('fetchData');
+const accountSelector = document.getElementById('accountSelector');
+const tableBody = document.getElementById('tableBody');
+
+let selectedAccountId;
+
+// Add click event listener to fetchData button
+fetchDataBtn.addEventListener('click', fetchData);
+
+// Add change event listener to account selector
+accountSelector.addEventListener('change', function() {
+  selectedAccountId = this.value;
+});
+
+// Google sign in function
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('Full Name: ' + profile.getName());
+    console.log('Email: ' + profile.getEmail());
+
+    // Load Analytics client library
+    gapi.load('client:analytics', function() {
+        console.log('Loaded client:analytics');
+        
+        // Get list of Google Analytics accounts
+        gapi.client.analytics.management.accounts.list().then(function(response) {
+            var accounts = response.result.items;
+            console.log('Google Analytics Accounts:', accounts);
+
+            // Enable account selector dropdown
+            accountSelector.disabled = false;
+
+            // Add accounts to dropdown
+            accounts.forEach(account => {
+                const option = document.createElement('option');
+                option.value = account.id; // Use account ID as option value
+                option.textContent = account.name; // Use account name as option text
+                accountSelector.appendChild(option);
+            });
+        });
+    });
+}
+
+function fetchData() {
+    fetchDataBtn.textContent = 'Loading...';
+    fetchDataBtn.disabled = true;
+
+    // Make sure a Google Analytics Account ID is selected
+    if (!selectedAccountId) {
+        console.error('Error: No Google Analytics Account ID selected');
+        fetchDataBtn.textContent = 'Fetch Data';
+        fetchDataBtn.disabled = false;
+        return;
+    }
+
+    // Define the request
+    let request = {
+        'reportRequests': [
+        {
+            'viewId': selectedAccountId,
+            'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
+            'metrics': [{'expression': 'ga:sessions'}],
+            'dimensions': [{'name': 'ga:source'}]
+        }]
+    };
+
+    // Fetch data from Google Analytics API
+    gapi.client.analyticsreporting.reports.batchGet(request)
+        .then(response => {
+            // Handle the response
+            let rows = response.result.reports[0].data.rows;
+
+            // Clear the table
+            tableBody.innerHTML = '';
+
+            // Populate the table
+            rows.forEach(row => {
+                const newRow = document.createElement('tr');
+                const sourceCell = document.createElement('td');
+                sourceCell.textContent = row.dimensions[0];
+                const sessionsCell = document.createElement('td');
+                sessionsCell.textContent = row.metrics[0].values[0];
+                newRow.appendChild(sourceCell);
+                newRow.appendChild(sessionsCell);
+                tableBody.appendChild(newRow);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+        .finally(() => {
+            fetchDataBtn.textContent = 'Fetch Data';
+            fetchDataBtn.disabled = false;
+        });
+}
